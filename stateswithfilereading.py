b4a6cd6c319d40c9ecb5a1358667c4f5b2140b50
JessicaGarson <jessica.garson@gmail.com>
2014-03-22 14:04:50 -0400

    states

---
+++ stateswithfilereading.py
@@ -0,0 +1,15 @@
+
+with open("statespyclass.csv", "r") as states_file:		
+		states = states_file.read().split("\n")
+
+		for index, state in  enumerate(states):
+				states[index] = state.split(",")
+
+print "<select>"
+
+
+for state in states:
+	print """\n \t <option value= "{0}">"{1}"</option> \n""" .format(state[0], state[1]) 
+
+print "</select>"
+
